9084dc4ee3f33eaca98029fb408ebbceddd29c4b
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-10-25 16:48:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+// Getting category
+const category = require('./data/category.json');
+
+app.get('/category', (req, res) => {
+    res.send(category);
+})
+
+app.listen(port, () => {
+    console.log('Course server is running on port: ', port);
+})
